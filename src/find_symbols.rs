db6a5d7f5eff205549ebad015d0d1462b335bb8e
Ilya Bylich <ibylich@gmail.com>
2023-03-20 20:21:17 +0100

    handle binary mach-o

--- src/find_symbols.rs
+++ src/find_symbols.rs
@@ -1,14 +1,32 @@
-use goblin::Object;
+use goblin::{mach::Mach, Object};
 
-fn find_all_symbols(data: &[u8]) -> Option<Vec<&str>> {
+fn find_all_symbols(data: &[u8]) -> Option<Vec<String>> {
     match Object::parse(&data).ok()? {
-        Object::Elf(elf) => elf.strtab.to_vec().ok(),
+        Object::Elf(elf) => {
+            let syms = elf
+                .strtab
+                .to_vec()
+                .ok()?
+                .into_iter()
+                .map(ToString::to_string)
+                .collect::<Vec<_>>();
+            Some(syms)
+        }
         Object::PE(pe) => {
             println!("pe: {:#?}", &pe);
             None
         }
-        Object::Mach(mach) => {
-            println!("mach: {:#?}", &mach);
+        Object::Mach(Mach::Binary(mach)) => {
+            let syms = mach
+                .exports()
+                .ok()?
+                .into_iter()
+                .map(|e| e.name)
+                .collect::<Vec<_>>();
+            Some(syms)
+        }
+        Object::Mach(Mach::Fat(mach)) => {
+            println!("fat mach: {:#?}", &mach);
             None
         }
         Object::Archive(archive) => {
@@ -22,16 +40,32 @@ fn find_all_symbols(data: &[u8]) -> Option<Vec<&str>> {
     }
 }
 
-pub fn find_test_symbols(data: &[u8]) -> Option<Vec<&str>> {
+pub fn find_test_symbols(data: &[u8]) -> Option<Vec<String>> {
     let mut symbols = find_all_symbols(data)?;
-    symbols.retain(|sym| sym.starts_with("__test_"));
+    symbols.retain(|sym| sym.contains("__test_"));
     Some(symbols)
 }
 
-#[test]
-fn test_elf() {
-    let data = std::fs::read("fixtures/elf.so").unwrap();
-    let mut symbols = find_test_symbols(&data).unwrap();
-    symbols.sort_unstable();
-    assert_eq!(symbols, vec!["__test_fail", "__test_pass"]);
+#[cfg(test)]
+mod tests {
+    use super::find_test_symbols;
+
+    fn read_fixture(path: &str) -> Vec<String> {
+        let data = std::fs::read(path).unwrap();
+        let mut symbols = find_test_symbols(&data).unwrap();
+        symbols.sort_unstable();
+        symbols
+    }
+
+    #[test]
+    fn test_elf() {
+        let symbols = read_fixture("fixtures/elf.so");
+        assert_eq!(symbols, vec!["__test_fail", "__test_pass"]);
+    }
+
+    #[test]
+    fn test_mach_o() {
+        let symbols = read_fixture("fixtures/mach-o.dylib");
+        assert_eq!(symbols, vec!["___test_fail", "___test_pass"]);
+    }
 }
