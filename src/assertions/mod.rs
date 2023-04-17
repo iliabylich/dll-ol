90417049fa6b0e7bf5c075fb7dc4b0ddfe3fd81e
Ilya Bylich <ibylich@gmail.com>
2023-04-17 19:51:07 +0200

    implement assert_{true,false} assertions

--- src/assertions/mod.rs
+++ src/assertions/mod.rs
@@ -1,2 +1,21 @@
 mod gen;
-pub use gen::trigger_inclusion;
+use gen::trigger_inclusion as gen_trigger_inclusion;
+
+#[no_mangle]
+pub extern "C" fn assert_true(_value: bool) {
+    todo!()
+}
+#[no_mangle]
+pub extern "C" fn assert_false(_value: bool) {
+    todo!()
+}
+
+pub fn trigger_inclusion() -> usize {
+    [
+        assert_true as *mut std::ffi::c_void as usize,
+        assert_false as *mut std::ffi::c_void as usize,
+    ]
+    .into_iter()
+    .fold(0, |acc: usize, e| acc.wrapping_add(e))
+        + gen_trigger_inclusion()
+}
