4c5609faad303bfd1831d9a17412c73ab2f6f5d2
Ilya Bylich <ibylich@gmail.com>
2023-04-17 19:27:10 +0200

    properly cast signed/insigned ints

--- codegen.rb
+++ codegen.rb
@@ -1,37 +1,67 @@
+class Type < Struct.new(:c, :rust_name, :top_c_type, :format_string, :sample1, :sample2)
+  def to_c_id
+    c.gsub(' *', '_ptr').gsub(' ', '_')
+  end
+end
+
+class SignedIntType < Type
+  def initialize(c, rust_name)
+    super(c, rust_name, 'int64_t', '%lld', '-1', '-2')
+  end
+end
+
+class UnsignedIntType < Type
+  def initialize(c, rust_name)
+    super(c, rust_name, 'uint64_t', '%llu', '1', '2')
+  end
+end
+
+class FloatType < Type
+  def initialize(c, rust_name)
+    super(c, rust_name, 'long double', '%Lf', '42.0', '43.0')
+  end
+end
+
+class String
+  def fix_ptrs
+    gsub(' * ', ' *')
+  end
+end
+
 INTEGER_TYPES = [
-  'char',
-  'unsigned char',
-  'short',
-  'unsigned short',
-  'int',
-  'unsigned int',
-  'long',
-  'unsigned long',
-  'long long',
-  'unsigned long long',
-  'int8_t',
-  'uint8_t',
-  'int16_t',
-  'uint16_t',
-  'int32_t',
-  'uint32_t',
-  'int64_t',
-  'uint64_t',
-  'bool',
-  'size_t',
+  SignedIntType.new('char', 'i8'),
+  UnsignedIntType.new('unsigned char', 'u8'),
+  SignedIntType.new('short', 'i8'),
+  UnsignedIntType.new('unsigned short', 'u8'),
+  SignedIntType.new('int', 'i8'),
+  UnsignedIntType.new('unsigned int', 'u8'),
+  SignedIntType.new('long', 'i8'),
+  UnsignedIntType.new('unsigned long', 'u8'),
+  SignedIntType.new('long long', 'i8'),
+  UnsignedIntType.new('unsigned long long', 'u8'),
+  SignedIntType.new('int8_t', 'u8'),
+  UnsignedIntType.new('uint8_t', 'u8'),
+  SignedIntType.new('int16_t', 'u8'),
+  UnsignedIntType.new('uint16_t', 'u8'),
+  SignedIntType.new('int32_t', 'u8'),
+  UnsignedIntType.new('uint32_t', 'u8'),
+  SignedIntType.new('int64_t', 'u8'),
+  UnsignedIntType.new('uint64_t', 'u8'),
+  UnsignedIntType.new('bool', 'u8'),
+  UnsignedIntType.new('size_t', 'u8'),
 ]
 
 FLOAT_TYPES = [
-  'float',
-  'double',
-  'long double',
+  FloatType.new('float', 'f32'),
+  FloatType.new('double', 'f32'),
+  FloatType.new('long double', 'f32'),
 ]
 
 ALL_TYPES = [
   *INTEGER_TYPES,
   *FLOAT_TYPES,
-  'char *',
-  'void *',
+  Type.new('char *', 'TODO', 'char *', '%s', '"hello"', "'world'"),
+  Type.new('void *', 'TODO', 'void *', '%p', 'NULL', '(void *)42'),
 ]
 
 # headers/assertions.gen.h
@@ -41,9 +71,8 @@ File.open('headers/assertions.gen.h', 'w') do |f|
   f.puts '#include <stdint.h>'
   f.puts
   ALL_TYPES.each do |type|
-    typename = type.gsub(' *', '_ptr').gsub(' ', '_')
-    f.puts "void assert_eq_#{typename}(#{type} lhs, #{type} rhs);"
-    f.puts "void assert_ne_#{typename}(#{type} lhs, #{type} rhs);"
+    f.puts "void assert_eq_#{type.to_c_id}(#{type.c} lhs, #{type.c} rhs);"
+    f.puts "void assert_ne_#{type.to_c_id}(#{type.c} lhs, #{type.c} rhs);"
     f.puts
   end
 end
@@ -55,55 +84,31 @@ File.open('runner/assertions.gen.h', 'w') do |f|
   f.puts '#include <stdint.h>'
   f.puts '#include <stdio.h>'
   f.puts
-  INTEGER_TYPES.each do |type|
-    typename = type.gsub(' *', '_ptr').gsub(' ', '_')
-    f.puts "void assert_eq_#{typename}(#{type} lhs, #{type} rhs)"
+  ALL_TYPES.each do |type|
+    f.puts "void assert_eq_#{type.to_c_id}(#{type.c} lhs, #{type.c} rhs)".fix_ptrs
     f.puts "{"
-    f.puts "    printf(\"assert_eq_#{typename}(%llu, %llu)\\n\", (uint64_t)lhs, (uint64_t)rhs);"
+    f.puts "    printf(\"assert_eq_#{type.to_c_id}(#{type.format_string}, #{type.format_string})\\n\", (#{type.top_c_type})lhs, (#{type.top_c_type})rhs);"
     f.puts "}"
-    f.puts "void assert_ne_#{typename}(#{type} lhs, #{type} rhs)"
+    f.puts "void assert_ne_#{type.to_c_id}(#{type.c} lhs, #{type.c} rhs)".fix_ptrs
     f.puts "{"
-    f.puts "    printf(\"assert_ne_#{typename}(%llu, %llu)\\n\", (uint64_t)lhs, (uint64_t)rhs);"
-    f.puts "}"
-    f.puts
-  end
-  FLOAT_TYPES.each do |type|
-    typename = type.gsub(' *', '_ptr').gsub(' ', '_')
-    f.puts "void assert_eq_#{typename}(#{type} lhs, #{type} rhs)"
-    f.puts "{"
-    f.puts "    printf(\"assert_eq_#{typename}(%Lf, %Lf)\\n\", (long double)lhs, (long double)rhs);"
-    f.puts "}"
-    f.puts "void assert_ne_#{typename}(#{type} lhs, #{type} rhs)"
-    f.puts "{"
-    f.puts "    printf(\"assert_ne_#{typename}(%Lf, %Lf)\\n\", (long double)lhs, (long double)rhs);"
+    f.puts "    printf(\"assert_ne_#{type.to_c_id}(#{type.format_string}, #{type.format_string})\\n\", (#{type.top_c_type})lhs, (#{type.top_c_type})rhs);"
     f.puts "}"
     f.puts
   end
-  f.puts "void assert_eq_char_ptr(char *lhs, char *rhs)"
-  f.puts "{"
-  f.puts "    printf(\"assert_eq_char_ptr(%s, %s)\\n\", lhs, rhs);"
-  f.puts "}"
-  f.puts "void assert_ne_char_ptr(char *lhs, char *rhs)"
-  f.puts "{"
-  f.puts "    printf(\"assert_ne_char_ptr(%s, %s)\\n\", lhs, rhs);"
-  f.puts "}"
-  f.puts
 end
 
 # fixtures/assert_eq_all.gen.h
 File.open('fixtures/assert_eq_all.gen.h', 'w') do |f|
   INTEGER_TYPES.each do |type|
-    typename = type.gsub(' *', '_ptr').gsub(' ', '_')
-    f.puts "#{type} #{typename}_ = 42;"
-    f.puts "assert_eq_#{typename}(#{typename}_, #{typename}_);"
-    f.puts "assert_ne_#{typename}(#{typename}_, 0);"
+    f.puts "#{type.c} #{type.to_c_id}_ = 42;"
+    f.puts "assert_eq_#{type.to_c_id}(#{type.to_c_id}_, #{type.to_c_id}_);"
+    f.puts "assert_ne_#{type.to_c_id}(#{type.to_c_id}_, 0);"
     f.puts
   end
   FLOAT_TYPES.each do |type|
-    typename = type.gsub(' *', '_ptr').gsub(' ', '_')
-    f.puts "#{type} #{typename}_ = 42.0;"
-    f.puts "assert_eq_#{typename}(#{typename}_, #{typename}_);"
-    f.puts "assert_ne_#{typename}(#{typename}_, 0.0);"
+    f.puts "#{type.c} #{type.to_c_id}_ = 42.0;"
+    f.puts "assert_eq_#{type.to_c_id}(#{type.to_c_id}_, #{type.to_c_id}_);"
+    f.puts "assert_ne_#{type.to_c_id}(#{type.to_c_id}_, 0.0);"
     f.puts
   end
   f.puts "char *char_ptr_ = \"hello\";"
--- runner/assertions.gen.h
+++ runner/assertions.gen.h
@@ -5,11 +5,11 @@
 
 void assert_eq_char(char lhs, char rhs)
 {
-    printf("assert_eq_char(%llu, %llu)\n", (uint64_t)lhs, (uint64_t)rhs);
+    printf("assert_eq_char(%lld, %lld)\n", (int64_t)lhs, (int64_t)rhs);
 }
 void assert_ne_char(char lhs, char rhs)
 {
-    printf("assert_ne_char(%llu, %llu)\n", (uint64_t)lhs, (uint64_t)rhs);
+    printf("assert_ne_char(%lld, %lld)\n", (int64_t)lhs, (int64_t)rhs);
 }
 
 void assert_eq_unsigned_char(unsigned char lhs, unsigned char rhs)
@@ -23,11 +23,11 @@ void assert_ne_unsigned_char(unsigned char lhs, unsigned char rhs)
 
 void assert_eq_short(short lhs, short rhs)
 {
-    printf("assert_eq_short(%llu, %llu)\n", (uint64_t)lhs, (uint64_t)rhs);
+    printf("assert_eq_short(%lld, %lld)\n", (int64_t)lhs, (int64_t)rhs);
 }
 void assert_ne_short(short lhs, short rhs)
 {
-    printf("assert_ne_short(%llu, %llu)\n", (uint64_t)lhs, (uint64_t)rhs);
+    printf("assert_ne_short(%lld, %lld)\n", (int64_t)lhs, (int64_t)rhs);
 }
 
 void assert_eq_unsigned_short(unsigned short lhs, unsigned short rhs)
@@ -41,11 +41,11 @@ void assert_ne_unsigned_short(unsigned short lhs, unsigned short rhs)
 
 void assert_eq_int(int lhs, int rhs)
 {
-    printf("assert_eq_int(%llu, %llu)\n", (uint64_t)lhs, (uint64_t)rhs);
+    printf("assert_eq_int(%lld, %lld)\n", (int64_t)lhs, (int64_t)rhs);
 }
 void assert_ne_int(int lhs, int rhs)
 {
-    printf("assert_ne_int(%llu, %llu)\n", (uint64_t)lhs, (uint64_t)rhs);
+    printf("assert_ne_int(%lld, %lld)\n", (int64_t)lhs, (int64_t)rhs);
 }
 
 void assert_eq_unsigned_int(unsigned int lhs, unsigned int rhs)
@@ -59,11 +59,11 @@ void assert_ne_unsigned_int(unsigned int lhs, unsigned int rhs)
 
 void assert_eq_long(long lhs, long rhs)
 {
-    printf("assert_eq_long(%llu, %llu)\n", (uint64_t)lhs, (uint64_t)rhs);
+    printf("assert_eq_long(%lld, %lld)\n", (int64_t)lhs, (int64_t)rhs);
 }
 void assert_ne_long(long lhs, long rhs)
 {
-    printf("assert_ne_long(%llu, %llu)\n", (uint64_t)lhs, (uint64_t)rhs);
+    printf("assert_ne_long(%lld, %lld)\n", (int64_t)lhs, (int64_t)rhs);
 }
 
 void assert_eq_unsigned_long(unsigned long lhs, unsigned long rhs)
@@ -77,11 +77,11 @@ void assert_ne_unsigned_long(unsigned long lhs, unsigned long rhs)
 
 void assert_eq_long_long(long long lhs, long long rhs)
 {
-    printf("assert_eq_long_long(%llu, %llu)\n", (uint64_t)lhs, (uint64_t)rhs);
+    printf("assert_eq_long_long(%lld, %lld)\n", (int64_t)lhs, (int64_t)rhs);
 }
 void assert_ne_long_long(long long lhs, long long rhs)
 {
-    printf("assert_ne_long_long(%llu, %llu)\n", (uint64_t)lhs, (uint64_t)rhs);
+    printf("assert_ne_long_long(%lld, %lld)\n", (int64_t)lhs, (int64_t)rhs);
 }
 
 void assert_eq_unsigned_long_long(unsigned long long lhs, unsigned long long rhs)
@@ -95,11 +95,11 @@ void assert_ne_unsigned_long_long(unsigned long long lhs, unsigned long long rhs
 
 void assert_eq_int8_t(int8_t lhs, int8_t rhs)
 {
-    printf("assert_eq_int8_t(%llu, %llu)\n", (uint64_t)lhs, (uint64_t)rhs);
+    printf("assert_eq_int8_t(%lld, %lld)\n", (int64_t)lhs, (int64_t)rhs);
 }
 void assert_ne_int8_t(int8_t lhs, int8_t rhs)
 {
-    printf("assert_ne_int8_t(%llu, %llu)\n", (uint64_t)lhs, (uint64_t)rhs);
+    printf("assert_ne_int8_t(%lld, %lld)\n", (int64_t)lhs, (int64_t)rhs);
 }
 
 void assert_eq_uint8_t(uint8_t lhs, uint8_t rhs)
@@ -113,11 +113,11 @@ void assert_ne_uint8_t(uint8_t lhs, uint8_t rhs)
 
 void assert_eq_int16_t(int16_t lhs, int16_t rhs)
 {
-    printf("assert_eq_int16_t(%llu, %llu)\n", (uint64_t)lhs, (uint64_t)rhs);
+    printf("assert_eq_int16_t(%lld, %lld)\n", (int64_t)lhs, (int64_t)rhs);
 }
 void assert_ne_int16_t(int16_t lhs, int16_t rhs)
 {
-    printf("assert_ne_int16_t(%llu, %llu)\n", (uint64_t)lhs, (uint64_t)rhs);
+    printf("assert_ne_int16_t(%lld, %lld)\n", (int64_t)lhs, (int64_t)rhs);
 }
 
 void assert_eq_uint16_t(uint16_t lhs, uint16_t rhs)
@@ -131,11 +131,11 @@ void assert_ne_uint16_t(uint16_t lhs, uint16_t rhs)
 
 void assert_eq_int32_t(int32_t lhs, int32_t rhs)
 {
-    printf("assert_eq_int32_t(%llu, %llu)\n", (uint64_t)lhs, (uint64_t)rhs);
+    printf("assert_eq_int32_t(%lld, %lld)\n", (int64_t)lhs, (int64_t)rhs);
 }
 void assert_ne_int32_t(int32_t lhs, int32_t rhs)
 {
-    printf("assert_ne_int32_t(%llu, %llu)\n", (uint64_t)lhs, (uint64_t)rhs);
+    printf("assert_ne_int32_t(%lld, %lld)\n", (int64_t)lhs, (int64_t)rhs);
 }
 
 void assert_eq_uint32_t(uint32_t lhs, uint32_t rhs)
@@ -149,11 +149,11 @@ void assert_ne_uint32_t(uint32_t lhs, uint32_t rhs)
 
 void assert_eq_int64_t(int64_t lhs, int64_t rhs)
 {
-    printf("assert_eq_int64_t(%llu, %llu)\n", (uint64_t)lhs, (uint64_t)rhs);
+    printf("assert_eq_int64_t(%lld, %lld)\n", (int64_t)lhs, (int64_t)rhs);
 }
 void assert_ne_int64_t(int64_t lhs, int64_t rhs)
 {
-    printf("assert_ne_int64_t(%llu, %llu)\n", (uint64_t)lhs, (uint64_t)rhs);
+    printf("assert_ne_int64_t(%lld, %lld)\n", (int64_t)lhs, (int64_t)rhs);
 }
 
 void assert_eq_uint64_t(uint64_t lhs, uint64_t rhs)
@@ -212,10 +212,19 @@ void assert_ne_long_double(long double lhs, long double rhs)
 
 void assert_eq_char_ptr(char *lhs, char *rhs)
 {
-    printf("assert_eq_char_ptr(%s, %s)\n", lhs, rhs);
+    printf("assert_eq_char_ptr(%s, %s)\n", (char *)lhs, (char *)rhs);
 }
 void assert_ne_char_ptr(char *lhs, char *rhs)
 {
-    printf("assert_ne_char_ptr(%s, %s)\n", lhs, rhs);
+    printf("assert_ne_char_ptr(%s, %s)\n", (char *)lhs, (char *)rhs);
+}
+
+void assert_eq_void_ptr(void *lhs, void *rhs)
+{
+    printf("assert_eq_void_ptr(%p, %p)\n", (void *)lhs, (void *)rhs);
+}
+void assert_ne_void_ptr(void *lhs, void *rhs)
+{
+    printf("assert_ne_void_ptr(%p, %p)\n", (void *)lhs, (void *)rhs);
 }
 
