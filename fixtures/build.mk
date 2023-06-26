09baa764bc278b1438b5cf95aa515fac94618d5f
Ilya Bylich <ibylich@gmail.com>
2023-06-26 19:33:39 +0200

    always push fixtures to "fixtures-latest" release

--- fixtures/build.mk
+++ fixtures/build.mk
@@ -1,44 +1,43 @@
-FIXTURES_RELEASE = fixtures-v0.3
-BASE_FIXTURE_URL = https://github.com/iliabylich/dll-ol/releases/download/$(FIXTURES_RELEASE)
+BASE_FIXTURE_URL = https://github.com/iliabylich/dll-ol/releases/download/fixtures-latest
 
-MACH_O_DYLIB = fixtures/mach-o-binary.dylib
-ELF_SO = fixtures/elf.so
-PE_DLL = fixtures/pe.dll
+MACH_O_DYLIB = mach-o-binary.dylib
+ELF_SO = elf.so
+PE_DLL = pe.dll
 
 ifdef MACH_O_BUILD_LOCALLY
-GET_MACH_O = $(CC) -g -Wl,-undefined,dynamic_lookup -shared fixtures/tests.gen.c -o $(MACH_O_DYLIB)
+GET_MACH_O = $(CC) -g -Wl,-undefined,dynamic_lookup -shared fixtures/tests.gen.c -o fixtures/$(MACH_O_DYLIB)
 else
-GET_MACH_O = wget -q $(BASE_FIXTURE_URL)/mach-o-binary.dylib -O $(MACH_O_DYLIB)
+GET_MACH_O = wget -q $(BASE_FIXTURE_URL)/$(MACH_O_DYLIB) -O fixtures/$(MACH_O_DYLIB)
 endif
 
 ifdef SO_BUILD_LOCALLY
-GET_SO = $(CC) -g fixtures/tests.gen.c -shared -o $(ELF_SO)
+GET_SO = $(CC) -g fixtures/tests.gen.c -shared -o fixtures/$(ELF_SO)
 else
-GET_SO = wget -q $(BASE_FIXTURE_URL)/elf.so -O $(ELF_SO)
+GET_SO = wget -q $(BASE_FIXTURE_URL)/$(ELF_SO) -O fixtures/$(ELF_SO)
 endif
 
 ifdef PE_BUILD_LOCALLY
-GET_PE = cl.exe /std:c11 /D_USRDLL /D_WINDLL fixtures/tests.gen.c /MT /link /FORCE:UNRESOLVED /DLL /OUT:$(PE_DLL)
+GET_PE = cl.exe /std:c11 /D_USRDLL /D_WINDLL fixtures/tests.gen.c /MT /link /FORCE:UNRESOLVED /DLL /OUT:fixtures/$(PE_DLL)
 else
-GET_PE = wget -q $(BASE_FIXTURE_URL)/pe.dll -O $(PE_DLL)
+GET_PE = wget -q $(BASE_FIXTURE_URL)/$(PE_DLL) -O fixtures/$(PE_DLL)
 endif
 
-$(MACH_O_DYLIB): fixtures/tests.gen.c headers/assertions.gen.h
+fixtures/$(MACH_O_DYLIB): fixtures/tests.gen.c headers/assertions.gen.h
 	$(GET_MACH_O)
-CLEAN += $(MACH_O_DYLIB)
+CLEAN += fixtures/$(MACH_O_DYLIB)
 
-$(ELF_SO): fixtures/tests.gen.c headers/assertions.gen.h
+fixtures/$(ELF_SO): fixtures/tests.gen.c headers/assertions.gen.h
 	$(GET_SO)
-CLEAN += $(ELF_SO)
+CLEAN += fixtures/$(ELF_SO)
 
-$(PE_DLL): fixtures/tests.gen.c headers/assertions.gen.h
+fixtures/$(PE_DLL): fixtures/tests.gen.c headers/assertions.gen.h
 	$(GET_PE)
-CLEAN += $(PE_DLL)
+CLEAN += fixtures/$(PE_DLL)
 
 fixtures/tests.gen.c: codegen.rb
 	ruby codegen.rb
 CLEAN += fixtures/tests.gen.c
 
-download-fixtures: $(MACH_O_DYLIB) $(ELF_SO) $(PE_DLL)
+download-fixtures: fixtures/$(MACH_O_DYLIB) fixtures/$(ELF_SO) fixtures/$(PE_DLL)
 
 .PHONY: download-fixtures
